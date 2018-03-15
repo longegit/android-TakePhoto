6768c205d7123f2042870c37e8f88d906f4a8f2b
apple <apple@appledeiMac.local>
2018-03-15 17:36:30 +0800

    1

---
+++ takephoto_library/src/main/java/com/jph/takephoto/provider/TakePhotoFileProvider.java
@@ -0,0 +1,10 @@
+package com.jph.takephoto.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by apple on 2018/3/15.
+ */
+
+public class TakePhotoFileProvider extends FileProvider {
+}
